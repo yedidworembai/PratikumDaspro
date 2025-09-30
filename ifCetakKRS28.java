b116092f952af5596a1a0ac0e367832c1e923948
yedidworembai <paitworem@gmail.com>
2025-09-30 14:35:38 +0700

    percobaan 1

--- ifCetakKRS28.java
+++ ifCetakKRS28.java
@@ -5,9 +5,9 @@ public class ifCetakKRS28
      public static void main(String[] args){
         Scanner sc = new Scanner (System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
-        System.out.print("Apakah UKT sudah lunas? (true/false):  ");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-        if (uktLunas) {
+        if (uktLunas){
             System.out.println("pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
             sc.close();
