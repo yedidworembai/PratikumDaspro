79121396245fcec6e58545dc0f10d27049d4faa3
yedidworembai <paitworem@gmail.com>
2025-11-05 21:15:10 +0700

    modifikasi percobaan 3

--- jobsheet9/ArrayRataNilai28.java
+++ jobsheet9/ArrayRataNilai28.java
@@ -1,28 +1,45 @@
 package jobsheet9;
+
 import java.util.Scanner;
+
 public class ArrayRataNilai28 {
+
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-int [] nilaiMhs= new int [10];
-double total = 0;
-double rata2;
-int lulus = 0;
+        // Deklarasi Scanner untuk input
+        Scanner sc = new Scanner(System.in);
+        
+        // Deklarasi array untuk 10 nilai mahasiswa
+        int[] nilaiMhs = new int[10]; 
+        
+        // Variabel untuk total nilai, rata-rata, dan jumlah yang lulus
+        double total = 0;
+        double rata2;
+        int lulus = 0; 
 
-for (int i=0; i<nilaiMhs.length; i++){
-System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
-    nilaiMhs[i] = sc .nextInt();
-        total = total + nilaiMhs[i];
-        if (nilaiMhs[i] > 70){
-            lulus++;
+        // Loop untuk menerima input nilai dari 10 mahasiswa
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+            
+            // Menghitung total nilai
+            total = total + nilaiMhs[i];
+            
+            // Pengecekan kelulusan (nilai > 70)
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+            }
         }
-
+        
+        // Menghitung rata-rata keseluruhan
+        rata2 = total / nilaiMhs.length;
+        
+        // Menampilkan hasil
+        System.out.println("----------------------------------------");
+        System.out.println("Rata-rata nilai mahasiswa adalah :" + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus :" + lulus);
+        System.out.println("----------------------------------------");
+        
+        // Menutup Scanner
+        sc.close();
     }
-    rata2 = total/nilaiMhs.length;
-System.out.println("Rata-rata nilai mahasiswa adalah : "+rata2);
-System.out.println("Jumlah mahasiswa yang lulus : "+lulus); 
-
-    
-    sc.close();
-}   
-    
 }
