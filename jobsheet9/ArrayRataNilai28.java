a49901597ebcd2589dcc87f2091aff7588b96efd
yedidworembai <paitworem@gmail.com>
2025-11-05 08:57:51 +0700

    Modifikasi percobaan 3

--- jobsheet9/ArrayRataNilai28.java
+++ jobsheet9/ArrayRataNilai28.java
@@ -6,14 +6,23 @@ public class ArrayRataNilai28 {
 int [] nilaiMhs= new int [10];
 double total = 0;
 double rata2;
+int lulus = 0;
+
 for (int i=0; i<nilaiMhs.length; i++){
 System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
     nilaiMhs[i] = sc .nextInt();
         total = total + nilaiMhs[i];
-rata2 = total/nilaiMhs.length;
-System.out.println("Rata-rata nilai mahasiswa adalah : "+rata2);
+        if (nilaiMhs[i] > 70){
+            lulus++;
+        }
 
     }
+    rata2 = total/nilaiMhs.length;
+System.out.println("Rata-rata nilai mahasiswa adalah : "+rata2);
+System.out.println("Jumlah mahasiswa yang lulus : "+lulus); 
+
+    
+    sc.close();
 }   
     
 }
