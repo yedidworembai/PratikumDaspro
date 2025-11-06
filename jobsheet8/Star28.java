355411be794ba52046808c052411f8f790a2f4b8
yedidworembai <paitworem@gmail.com>
2025-11-06 11:10:50 +0700

    percobaan 1

---
+++ jobsheet8/Star28.java
@@ -0,0 +1,18 @@
+package jobsheet8;
+import java.util.Scanner;
+public class Star28 {
+
+
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+        System.out.print("*");
+        
+            }
+        sc.close();
+           
+        }
+
+}
