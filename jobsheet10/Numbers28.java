74d52407091a990f796b42bb257ee8b05e3c9e4a
yedidworembai <paitworem@gmail.com>
2025-11-14 00:09:04 +0700

    modifikasi percobaan 3

--- jobsheet10/Numbers28.java
+++ jobsheet10/Numbers28.java
@@ -1,16 +1,23 @@
-package jobsheet10;
+package jobsheet10;  
+import java.util.Arrays; 
+
 public class Numbers28 {
     
     public static void main(String[] args) {
         
-        
         int[][] myNumbers = new int[3][];
-        
         myNumbers[0] = new int[5]; 
-        myNumbers[1] = new int[3];
-        myNumbers[2] = new int[1];
-        
-        
-        
+        myNumbers[1] = new int[3]; 
+        myNumbers[2] = new int[1]; 
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Baris ke-" + (i+1) + ": " + Arrays.toString(myNumbers[i]));
+        }
+
+       
+        System.out.println("\n--- 4. Mencetak Panjang Setiap Baris ---");
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + " : " + myNumbers[i].length);
+        }
     }
 }
