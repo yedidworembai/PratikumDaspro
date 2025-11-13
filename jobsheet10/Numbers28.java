16c57bf049c2f46228a082b527417874aa817ae2
yedidworembai <paitworem@gmail.com>
2025-11-13 23:51:40 +0700

    percobaan 3

---
+++ jobsheet10/Numbers28.java
@@ -0,0 +1,16 @@
+package jobsheet10;
+public class Numbers28 {
+    
+    public static void main(String[] args) {
+        
+        
+        int[][] myNumbers = new int[3][];
+        
+        myNumbers[0] = new int[5]; 
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+        
+        
+        
+    }
+}
